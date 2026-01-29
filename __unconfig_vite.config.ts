5706db342792d147af57a458f0e16a9c3746a493
jiaozhen <jiaozhen@shangbendata.com.cn>
2026-01-29 10:25:23 +0800

    旋转地球添加

---
+++ __unconfig_vite.config.ts
@@ -0,0 +1,47 @@
+
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) { __unconfig_data = data };
+__unconfig_stub.default = (data = {}) => { __unconfig_data = data };
+/// <reference types="vitest" />
+
+import type { ConfigEnv, UserConfig } from 'vite';
+
+import { createViteBuild } from './build/vite/build';
+import { createViteCSS } from './build/vite/css';
+import { createViteEsbuild } from './build/vite/esbuild';
+import { createViteOptimizeDeps } from './build/vite/optimizeDeps';
+import { createVitePlugins } from './build/vite/plugin';
+import { createViteResolve } from './build/vite/resolve';
+import { createViteServer } from './build/vite/server';
+
+// https://vitejs.dev/config/
+const __unconfig_default =  (configEnv: ConfigEnv): UserConfig => {
+  const { mode, command } = configEnv;
+  // const root = process.cwd();
+
+  // const env = loadEnv(mode, root);
+
+  const isBuild = command === 'build';
+
+  return {
+    // 设为 false 可以避免 Vite 清屏而错过在终端中打印某些关键信息。命令行模式下请通过 --clearScreen false 设置。
+    clearScreen: true,
+    logLevel: 'info',
+    // esbuild
+    esbuild: createViteEsbuild(isBuild),
+    // 解析配置
+    resolve: createViteResolve(mode, __dirname),
+    // 插件配置
+    plugins: createVitePlugins(isBuild, configEnv),
+    // 服务配置
+    server: createViteServer(),
+    // 打包配置
+    build: createViteBuild(),
+    // 依赖优化配置
+    optimizeDeps: createViteOptimizeDeps(),
+    // css预处理配置
+    css: createViteCSS(),
+  };
+};
+
+if (typeof __unconfig_default === "function") __unconfig_default(...[{"command":"serve","mode":"development"}]);export default __unconfig_data;
